4095f44de4296ad535089676244ef936242137cc
Pavel Taruts <taruts@improveitgroup.com>
2017-05-11 14:19:34 +0700

    Директива PhoneValidatorDirective. Вешает валидаторы телефона на <input app-phone>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,6 +25,7 @@ import {OurBackend} from './server/backend/our-backend';
 import {OurServerApi} from './server/our-server-api';
 import {RegistrationStep1CanActivateGuard} from './pages/registration/step-1/registration-step-1-can-activate.guard';
 import {AccessRestrictedComponent} from './pages/access-denied/access-restricted.component';
+import {PhoneValidatorDirective} from './common/forms/inputs/phone/phone-validator.directive';
 
 const ROUTES: Routes = [
   {
@@ -79,6 +80,7 @@ const ROUTES: Routes = [
     DateValidatorDirective,
     EnumRadioGroupComponent,
     AccessRestrictedComponent,
+    PhoneValidatorDirective
   ],
   providers: [
     CookieService,
---
+++ src/app/common/forms/inputs/phone/phone-validator.directive.ts
@@ -0,0 +1,46 @@
+import {Directive, forwardRef, Input} from '@angular/core';
+import {AbstractControl, NG_VALIDATORS, ValidationErrors, Validator} from '@angular/forms';
+import {PhoneTextMaskService} from './phone-text-mask.service';
+
+@Directive({
+  selector: 'input[type="text"][app-phone][textMask]',
+  providers: [
+    {
+      provide: NG_VALIDATORS,
+      useExisting: forwardRef(() => PhoneValidatorDirective),
+      multi: true
+    }
+  ]
+})
+export class PhoneValidatorDirective implements Validator {
+
+  @Input('required')
+  required: string;
+
+  constructor(private phoneTextMaskService: PhoneTextMaskService) {
+  }
+
+  validate(c: AbstractControl): ValidationErrors {
+
+    const inputValue: string = c.value;
+
+    if (this.phoneTextMaskService.isEmpty(inputValue)) {
+
+      if (this.required !== undefined) {
+        return {phoneEmpty: 'Телефон должен быть заполнен'};
+      }
+
+    } else {
+
+      if (inputValue.charAt(1).match(/[0-8]/)) {
+        return {phoneFirstCharacter: 'Первая цифра должна быть 9'};
+      }
+
+      if (inputValue.match(/_/)) {
+        return {phoneIncomplete: 'Номер телефона заполнен не полностью'};
+      }
+    }
+
+    return null;
+  }
+}
