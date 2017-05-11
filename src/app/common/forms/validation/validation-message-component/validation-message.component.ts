a21191300d75e8167f0a4cc194b7fa8e8426e56d
Pavel Taruts <taruts@improveitgroup.com>
2017-05-11 12:08:34 +0700

    Адаптировали ValidationMessageComponent не только к ngModel, но и к formControlName

--- src/app/common/forms/validation/validation-message-component/validation-message.component.ts
+++ src/app/common/forms/validation/validation-message-component/validation-message.component.ts
@@ -1,5 +1,5 @@
 import {Component, Directive, Inject, InjectionToken, NgModule, OnInit, Optional} from '@angular/core';
-import {NgModel} from '@angular/forms';
+import {FormControl, FormControlName, NgModel} from '@angular/forms';
 import {BrowserModule} from '@angular/platform-browser';
 import {ValidationMessageService} from '../validation-message.service';
 
@@ -17,24 +17,48 @@ export const APP_VALIDATION_MESSAGES_PROVIDER = new InjectionToken<ValidationMes
   selector: '.form-group'
 })
 export class BootstrapFormGroupDirective {
-
-  ngModel: NgModel;
-
-  constructor() {
-  }
+  formControlName: string;
+  formControl: FormControl;
 }
 
 @Directive({
-  selector: '[ngModel]'
+  selector: '[ngModel],[formControlName]'
 })
-export class NgModelSpyDirective {
+export class SpyFormControlNameDirective implements OnInit {
 
-  constructor(private ngModel: NgModel,
+  constructor(@Optional() private ngModel: NgModel,
+              @Optional() private formControlName: FormControlName,
               @Optional() private bootstrapFormGroupDirective: BootstrapFormGroupDirective) {
 
-    if (bootstrapFormGroupDirective != null) {
-      bootstrapFormGroupDirective.ngModel = ngModel;
+    console.log('SpyFormControlNameDirective.constructor');
+  }
+
+  ngOnInit(): void {
+
+    let formControlName: string;
+    let formControl: FormControl;
+
+    console.log('SpyFormControlNameDirective.ngOnInit, begin');
+
+    if (this.ngModel != null) {
+      formControlName = this.ngModel.name;
+      formControl = this.ngModel.control;
+    } else if (this.formControlName != null) {
+      formControlName = this.formControlName.name;
+      formControl = this.formControlName.control;
+    } else {
+      throw new Error('Что-то не так. Если мы здесь, то либо ngModel, либо formControlName должно быть не null');
     }
+
+    console.log('SpyFormControlNameDirective.ngOnInit, formControlName = ' + formControlName);
+    console.log('SpyFormControlNameDirective.ngOnInit, formControl = ' + formControl);
+
+    if (this.bootstrapFormGroupDirective != null) {
+      this.bootstrapFormGroupDirective.formControlName = formControlName;
+      this.bootstrapFormGroupDirective.formControl = formControl;
+    }
+
+    console.log('SpyFormControlNameDirective.ngOnInit, end');
   }
 }
 
@@ -45,19 +69,22 @@ export class NgModelSpyDirective {
 })
 export class ValidationMessageComponent implements OnInit {
 
-  messages: { [key: string]: string };
+  messages: {
+    [key: string]: string
+  };
 
-  ngModel: NgModel;
+  formControlName: string;
+  formControl: FormControl;
 
   constructor(public validationMessageService: ValidationMessageService,
               @Inject(APP_VALIDATION_MESSAGES_PROVIDER) private validationMessagesProvider: ValidationMessagesProvider,
               private bootstrapFormGroupDirective: BootstrapFormGroupDirective) {
-
-    this.ngModel = bootstrapFormGroupDirective.ngModel;
   }
 
   ngOnInit() {
-    this.messages = this.validationMessagesProvider.getMessages(this.ngModel.name);
+    this.formControlName = this.bootstrapFormGroupDirective.formControlName;
+    this.formControl = this.bootstrapFormGroupDirective.formControl;
+    this.messages = this.validationMessagesProvider.getMessages(this.formControlName);
   }
 }
 
@@ -67,12 +94,12 @@ export class ValidationMessageComponent implements OnInit {
   ],
   declarations: [
     BootstrapFormGroupDirective,
-    NgModelSpyDirective,
+    SpyFormControlNameDirective,
     ValidationMessageComponent
   ],
   exports: [
     BootstrapFormGroupDirective,
-    NgModelSpyDirective,
+    SpyFormControlNameDirective,
     ValidationMessageComponent
   ]
 })
