e66a1a8acee2527ecc797b2c5cc1e6e78f90d809
Pavel Taruts <taruts@improveitgroup.com>
2017-05-17 15:31:03 +0700

    Чистим AppModule

--- src/app/_modules/app/app.module.ts
+++ src/app/_modules/app/app.module.ts
@@ -1,22 +1,20 @@
 import {NgModule} from '@angular/core';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {BrowserModule} from '@angular/platform-browser';
 import {RouterModule, Routes} from '@angular/router';
 import {CookieService} from 'angular2-cookie/core';
 import {CacheModule} from '../../common/cache/cache.module';
 import {ModalModule} from '../../common/components/modal/modal.module';
-import {ValidationMessageComponentModule} from '../../common/forms/validation/validation-message-component/validation-message.component';
 import {AccessRestrictedModule} from '../../pages/access-denied/access-restricted.module';
 import {ErrorModule} from '../../pages/error/error.module';
 import {HomeModule} from '../../pages/home/home.module';
 import {LoginModule} from '../../pages/login/login.module';
-import {MainComponent} from './main.component';
-import {OurBackend} from '../../server/backend/our-backend';
-import {OurServerApi} from '../../server/our-server-api';
+import {ProfileModule} from '../../pages/profile/profile.module';
 import {RegistrationStep1Module} from '../../pages/registration/step-1/registration-step-1.module';
 import {RegistrationStep2Module} from '../../pages/registration/step-2/registration-step-2.module';
 import {RegistrationStep3Module} from '../../pages/registration/step-3/registration-step-3.module';
-import {ProfileModule} from '../../pages/profile/profile.module';
+import {OurBackend} from '../../server/backend/our-backend';
+import {OurServerApi} from '../../server/our-server-api';
+import {MainComponent} from './main.component';
 
 const ROUTES: Routes = [
   HomeModule.ROUTE,
@@ -41,20 +39,19 @@ const ROUTES: Routes = [
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule,
-    ReactiveFormsModule,
     RouterModule.forRoot(ROUTES),
-    ValidationMessageComponentModule,
     CacheModule,
     ModalModule,
-    AccessRestrictedModule,
+
+    // Страницы
     HomeModule,
-    ErrorModule,
-    LoginModule,
     RegistrationStep1Module,
     RegistrationStep2Module,
     RegistrationStep3Module,
     ProfileModule,
+    LoginModule,
+    AccessRestrictedModule,
+    ErrorModule,
   ],
   declarations: [
     MainComponent,
